33920337541a564216d5c8b39420a58747592fc2
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-04-30 20:32:43 -0500

    refactoring

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import PageHeader from "./Components/PageHeader/PageHeader";
 import SearchBar from "./Components/SearchBar/SearchBar";
-// import EmployeeCard from "./Components/EmployeeCard/EmployeeCard";
-// import { Row, Col } from "react-bootstrap";
-// import "./App.css";
+
 function App() {
   return (
     <div className="App">
